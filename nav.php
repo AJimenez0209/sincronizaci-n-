38a8b25a30a17af3abe08a34a048986e70afa15a
AaronJ <jimenezaaron5@gmail.com>
2022-07-11 00:31:22 -0400

    modificando localmente el archivo nav

---
+++ nav.php
@@ -0,0 +1,8 @@
+<nav>
+    <ol>
+        <li><a href="#">Home</a></i>
+        <li><a href="#">Quienes Somos</a></i>
+        <li><a href="#">Nosotrs</a></i>
+        <li><a href="#">Contacto</a></i>
+    </ol>
+</nav>
